ecd384a02ec1695702d4865f53e8194595565f87
BredyByte <correodemimobil@gmail.com>
2024-11-18 18:57:12 +0100

    feat: Getting active no local ethernet interface

--- src/main.c
+++ src/main.c
@@ -1,5 +1,39 @@
 #include "malcolm.h"
 
+int get_free_interface(t_network_data *data) {
+    struct ifaddrs *ifaddr, *ifa;
+    char ipstr[INET_ADDRSTRLEN];
+
+    if (getifaddrs(&ifaddr) == -1) return -1;
+
+    for (ifa = ifaddr; ifa != NULL; ifa = ifa->ifa_next) {
+
+        // Skiping interfaces without IPv4 addresses OR with disabled status. IFF_UP == active
+        if (ifa->ifa_addr == NULL || (ifa->ifa_flags & IFF_UP) == 0 || ifa->ifa_addr->sa_family != AF_INET) {
+            continue;
+        }
+
+        void *addr = &((struct sockaddr_in *)ifa->ifa_addr)->sin_addr;
+        inet_ntop(AF_INET, addr, ipstr, sizeof(ipstr));
+
+        printf("ip is: %s \n", ipstr);
+
+        if (ft_strncmp(ipstr, "127.", 4) == 0) {
+            continue;
+        }
+
+        ft_strncpy(data->interface_name, ifa->ifa_name, IF_NAMESIZE);
+
+        printf("Found active interface: %s with IP %s\n", ifa->ifa_name, ipstr);
+
+        freeifaddrs(ifaddr);
+        return 0;
+    }
+
+    freeifaddrs(ifaddr);
+    return -1;
+}
+
 int main(int argc, const char **argv) {
     t_network_data data;
 
@@ -13,12 +47,20 @@ int main(int argc, const char **argv) {
         return 1;
     }
 
+    // Initialization
+    if (get_free_interface(&data) != 0) {
+        fprintf(stderr, "Error: Could not find a free network interface.\n");
+        return 1;
+    }
+
+    printf("Found interface: %s with IP: %s\n", data.interface_name, data.source_ip);
+
     printf("Source IP: %s\n", data.source_ip);
     printf("Source MAC: %s\n", data.source_mac);
     printf("Target IP: %s\n", data.target_ip);
     printf("Target MAC: %s\n", data.target_mac);
+    printf("Active interface %s\n", data.interface_name);
 
-	// Initialization
 	// Processing
 	// Cleanup
 
